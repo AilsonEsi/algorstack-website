c9fe39db307048d74fbd912d84e8f57bb01a34a4
Ailson Moreira <35077956+AilsonEsi@users.noreply.github.com>
2025-06-19 17:09:53 -0100

    change api base endpoint

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.algorstack.com/v1', // Replace with your production API URL
+  //apiUrl: 'https://api.algorstack.com/v1', // Replace with your production API URL
+  apiUrl: 'https://algorstack-api.onrender.com/v1'
+  
 };
